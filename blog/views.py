b161a4cd84896a434598e56e155363927af3ba8f
irzelindo <irzelindo.salvador@gmail.com>
2020-08-01 19:00:43 +0200

    First commit

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Irzelindo Salvador',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Sherly Salvador',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 21, 2018'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
