eaa290ae7e963e11d6e57d2c03de50d02c214e41
irzelindo <irzelindo.salvador@gmail.com>
2020-08-03 14:46:54 +0200

    Add resize image functionality

--- users/forms.py
+++ users/forms.py
@@ -10,9 +10,15 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class UserUpadateForm(forms.ModelForm):
+class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
