e472d882de22a2ad5619177c8c43e308f7c9b84d
irzelindo <irzelindo.salvador@gmail.com>
2020-08-07 20:55:51 +0200

    Add password reset functionality

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,3 +131,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'irzelindo.salvador@gmail.com' #os.environ.username('EMAIL')
+EMAIL_HOST_PASSWORD = 'vinkyerrhopdsalu' #os.environ.get('PASSWORD')
