a0474fd9197a6dde271f6c9387d5b43bac2b1bab
irzelindo <irzelindo.salvador@gmail.com>
2020-08-09 18:23:11 +0200

    Change settings.py file email configurattion to use environment variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'irzelindo.salvador@gmail.com' #os.environ.username('EMAIL')
-EMAIL_HOST_PASSWORD = 'vinkyerrhopdsalu' #os.environ.get('PASSWORD')
+EMAIL_HOST_USER = os.environ.username('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
